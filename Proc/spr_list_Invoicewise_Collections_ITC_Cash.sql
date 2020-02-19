487c32f63a0d5a14de3fd8cab2fe2f1f4f29aa85
Manickam <manickam.g@inexgen.com>
2020-02-19 11:42:42 +0530

    Collection Report Payment Mode Filter. Invoice Print Details Base Version

--- Proc/spr_list_Invoicewise_Collections_ITC_Cash.sql
+++ Proc/spr_list_Invoicewise_Collections_ITC_Cash.sql
@@ -1,5 +1,5 @@
 --Exec ARC_Insert_ReportData 374, 'Collections Report', 1, ' spr_list_Invoicewise_Collections_ITC_Cash', 'View InvoiceWise Collections', 151, 76, 1, 2, 0, 0, 3, 0, 0, 0, 252, 'No'
---exec spr_list_Invoicewise_Collections_ITC_Cash '%','2020-02-01 00:00:00','2020-02-10 23:59:59','%'
+--exec spr_list_Invoicewise_Collections_ITC_Cash '%','2020-02-17 00:00:00','2020-02-19 23:59:59','Cash'
 IF EXISTS(SELECT Top 1 1 FROM sys.objects WHERE Name = N'spr_list_Invoicewise_Collections_ITC_Cash')
 BEGIN
 	DROP PROC [spr_list_Invoicewise_Collections_ITC_Cash]
@@ -8,7 +8,10 @@ GO
 CREATE PROCEDURE [dbo].[spr_list_Invoicewise_Collections_ITC_Cash] (@Salesman nVarchar(2550), @FromDate datetime,@ToDate datetime,@PaymentMode nVarchar(50))        
 As        
      SET DATEFORMAT DMY
-Create table #tmpSalesMan(SalesmanID INT)      
+Create table #tmpSalesMan(SalesmanID INT)     
+
+if @PaymentMode=N'AllCollectionType'      
+ set @PaymentMode=N'%'
       
 if @Salesman = '%'             
 begin      
@@ -32,7 +35,14 @@ SELECT 1,
 		CO.CollectionId,
 		CO.CollectionAmount,
 		CO.InvoiceReference,
-		(SELECT Top 1 Value FROM Paymentmode WITH (NOLOCK) WHERE mode = CO.Paymentmode) PaymentType,
+		((case cast(CO.Paymentmode as nvarchar) 
+		 when '0' then  'Cash'           
+		 when '1'  then 'Cheque'             
+		 when  '2' then 'DD'            
+		 when '3'   then 'Credit Card'           
+		 when  '4'  then 'Bank Transfer'        
+		 when '5' then 'Coupon'            
+		 else 'Others' end)) PaymentType,
 		Case CO.DocumentID When 0 then 'Normal Collection' else 'Handheld Collection' End [CollectionMode],
 		CO.ChequeDate,
 		CO.ChequeNumber,
@@ -50,8 +60,18 @@ SELECT 1,
 	V_ARC_Collections CO WITH (NOLOCK)
 	JOIN #tmpSalesMan S WITH (NOLOCK) ON S.SalesmanID = CO.SalesmanID
 	WHERE dbo.StripDateFromTime(CO.CollectionDate) Between @FromDate AND @ToDate
+	AND cast(CO.Paymentmode as nvarchar) like           
+	 (case @PaymentMode when 'Cash' then '0'           
+	 when 'Cheque' then '1'           
+	 when 'DD' then '2'           
+	 when 'Credit Card' then '3'           
+	 when 'Bank Transfer' then '4'        
+	 when 'Coupon' then '5'           
+	 else '%' end)
 	
 
 	Drop table #tmpSalesMan
   
 SET QUOTED_IDENTIFIER OFF
+Go
+
