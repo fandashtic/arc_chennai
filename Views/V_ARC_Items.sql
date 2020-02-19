1470961fb6cbad5a5d6c01532f00eafb07b1779a
Manickam <manickam.g@inexgen.com>
2020-02-19 19:13:00 +0530

    Upgrade View Items.

--- Views/V_ARC_Items.sql
+++ Views/V_ARC_Items.sql
@@ -1,4 +1,4 @@
- --Select * from V_ARC_Items_BatchDetails T WHERE T.Product_Code = 'FD2125N' AND CAST(T.Batch_Code AS VARCHAR(255)) = '06A300719-30'
+ --Select * from V_ARC_Items T WHERE T.Product_Code = 'FD2125N' AND CAST(T.Batch_Code AS VARCHAR(255)) = '06A300719-30'
  IF EXISTS(SELECT * FROM sys.objects WHERE Name = N'V_ARC_Items')
 BEGIN
     DROP VIEW V_ARC_Items
@@ -13,7 +13,15 @@ SELECT
 	dbo.fn_Arc_GetCategory(P.Category) Category, 
 	dbo.fn_Arc_GetItemFamily(P.ItemFamily) ItemFamily, 
 	dbo.fn_Arc_GetItemSubFamily(P.ItemSubFamily) ItemSubFamily, 
-	dbo.fn_Arc_GetItemGroup(P.ItemGroup) ItemGroup
+	dbo.fn_Arc_GetItemGroup(P.ItemGroup) ItemGroup,
+	IC4.Category_Name [Manufacture],
+	IC3.Category_Name [Division],
+	IC2.Category_Name [SubCategory],
+	IC1.Category_Name [MarketSKU]
 from Items I WITH (NOLOCK)
+JOIN ItemCategories IC1 WITH (NOLOCK) ON I.CategoryID = IC1.CategoryID
+JOIN ItemCategories IC2 WITH (NOLOCK) ON IC2.CategoryID = IC1.ParentID
+JOIN ItemCategories IC3 WITH (NOLOCK) ON IC3.CategoryID = IC2.ParentID 
+JOIN ItemCategories IC4 WITH (NOLOCK) ON IC4.CategoryID = IC3.ParentID
 FULL OUTER JOIN Product_Mappings P WITH (NOLOCK) ON P.Product_Code = I.Product_Code
 GO
--- Views/V_ARC_Items_BatchDetails.sql
+++ Views/V_ARC_Items_BatchDetails.sql
@@ -13,16 +13,30 @@ SELECT
 	dbo.fn_Arc_GetCategory(P.Category) Category, 
 	dbo.fn_Arc_GetItemFamily(P.ItemFamily) ItemFamily, 
 	dbo.fn_Arc_GetItemSubFamily(P.ItemSubFamily) ItemSubFamily, 
-	dbo.fn_Arc_GetItemGroup(P.ItemGroup) ItemGroup,
+	dbo.fn_Arc_GetItemGroup(P.ItemGroup) ItemGroup, 
+	IC4.Category_Name [Manufacture],
+	IC3.Category_Name [Division],
+	IC2.Category_Name [SubCategory],
+	IC1.Category_Name [MarketSKU],
 	B.Batch_Code,
 	B.Batch_Number,
 	B.Quantity,
+	B.SalePrice,
+	B.PurchasePrice,
+	B.PTR,
 	B.GRN_ID,
 	B.Expiry,
 	(CASE WHEN ISNULL(B.Damage, 0) > 0 THEN 1 ELSE 0 END) IsDamage,
 	B.DamagesReason,
-	B.UOMQty
+	B.UOMQty,
+	B.CreationDate,
+	(Case When ISNULL(I.ReportingUOM, 0) = 0 THEN 1 ELSE I.ReportingUOM END) ReportingUOM,
+	DATEDIFF(d, B.CreationDate, Getdate()) [Aging]
 from Items I WITH (NOLOCK)
 FULL OUTER JOIN Product_Mappings P WITH (NOLOCK) ON P.Product_Code = I.Product_Code
 FULL OUTER JOIN Batch_Products B WITH (NOLOCK) ON B.Product_Code = I.Product_Code
+JOIN ItemCategories IC1 WITH (NOLOCK) ON I.CategoryID = IC1.CategoryID
+JOIN ItemCategories IC2 WITH (NOLOCK) ON IC2.CategoryID = IC1.ParentID
+JOIN ItemCategories IC3 WITH (NOLOCK) ON IC3.CategoryID = IC2.ParentID 
+JOIN ItemCategories IC4 WITH (NOLOCK) ON IC4.CategoryID = IC3.ParentID
 GO
