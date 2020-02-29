9721407b2d4c8c571c361184478e060a9f3da51e
Manickam <manickam.g@inexgen.com>
2020-02-29 10:04:20 +0530

    Add Customer Category.

--- Views/V_CUSTOMERS.sql
+++ Views/V_CUSTOMERS.sql
@@ -1,4 +1,4 @@
---SELECT * FROM V_CUSTOMERS
+--SELECT * FROM V_CUSTOMERS --where CustomerId = 'ARCBAK139'
 IF EXISTS(SELECT * FROM sys.objects WHERE Name = N'V_CUSTOMERS')
 BEGIN
     DROP VIEW V_CUSTOMERS
@@ -6,9 +6,10 @@ END
 GO
 CREATE VIEW V_CUSTOMERS
 AS
-select S.SalesmanID, S.Salesman_Name, B.BeatID, B.Description, C.CustomerId, C.Company_Name
+select Distinct S.SalesmanID, S.Salesman_Name, dbo.fn_Arc_GetSalesmanCategory(S.SalesmanID) SalesmanCategory,
+B.BeatID, B.Description, C.CustomerId, C.Company_Name, dbo.fn_Arc_GetCustomerCategory(C.CustomerId) CategoryGroup, dbo.fn_Arc_GetCustomerGroup(C.CustomerId) [Group]
 FROM Customer C WITH (NOLOCK)
-JOIN Beat_Salesman BS ON BS.CustomerID = C.CustomerID
-JOIN Beat B WITH (NOLOCK) ON B.BeatID = BS.BeatId AND B.Active = 1
-JOIN Salesman S WITH (NOLOCK) ON BS.SalesmanID = S.SalesmanID AND S.Active = 1
+LEFT OUTER JOIN Beat_Salesman BS ON BS.CustomerID = C.CustomerID
+LEFT OUTER JOIN Beat B WITH (NOLOCK) ON B.BeatID = BS.BeatId AND B.Active = 1
+LEFT OUTER JOIN Salesman S WITH (NOLOCK) ON BS.SalesmanID = S.SalesmanID AND S.Active = 1
 GO
